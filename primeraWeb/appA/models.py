5c5b4a19af0456b6f2960c6c3e291b16d639e6b8
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2025-06-23 07:12:48 +0200

    Avanzando en la generación de modelos (tablas)

--- primeraWeb/appA/models.py
+++ primeraWeb/appA/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha = models.DateTimeField()
+    nombreEmpleado = models.ForeignKey('Empleados') #conecto con la otra tabla o modelo
+
+    def __str__(self):
+        return f"Cliente: {self.nombre} -Vendedor: {self.nombreEmpleado.nombre} - [{self.fecha.strftime('%Y-%m-%d %H:%M:%S')}]"
+        # Devuelve el nombre del cliente, el nombre del empleado y la fecha en un formato legible
+
+
+class Empleados(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha = models.DateTimeField()
+
+    def __str__(self):
+        return self.nombre
